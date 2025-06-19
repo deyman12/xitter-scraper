634bd1597b8f773c2ea86a706270541a9e99ca27
deyman was here <128112289+deyman12@users.noreply.github.com>
2025-06-20 02:14:30 +0700

    chore: add default build extension dir

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-	srcDir: "src",
-	extensionApi: "chrome",
-	modules: ["@wxt-dev/module-svelte"],
-	manifestVersion: 3,
-	manifest: {
-		version: "3.0.0",
-		description:
-			"A browser extension that lets you download high-quality images from X/Twitter profiles in bulk.",
-		host_permissions: undefined,
-		permissions: [],
-	},
+  srcDir: "src",
+  outDir: "dist",
+  extensionApi: "chrome",
+  modules: ["@wxt-dev/module-svelte"],
+  manifestVersion: 3,
+  manifest: {
+    version: "3.0.0",
+    description:
+      "A browser extension that lets you download high-quality images from X/Twitter profiles in bulk.",
+    host_permissions: undefined,
+    permissions: [],
+  },
 });
